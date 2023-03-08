5184dd075077771dcdbf01c63462584659a1cb80
Dominik Plavka <domino.plavka@gmail.com>
2023-03-08 15:07:16 +0100

    Created data flow

---
+++ frontend/src/components/services/weatherServices.js
@@ -0,0 +1,56 @@
+const apiKey = '9139bc6d00ad8fa87da75f7489652eef';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/';
+
+//'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
+
+const getWeatherData = (infoType, searchParams) => {
+    const url = new URL(baseUrl + infoType);
+    url.search = new URLSearchParams({ ...searchParams, appid: apiKey });
+
+    return fetch(url)
+        .then((res) => res.json())
+};
+
+const formatCurrentWeather = (data) => {
+    const {
+        coord: { lat, lon },
+        main: { temp, feels_like, temp_min, temp_max, humidity },
+        name,
+        dt,
+        sys: { country, sunrise, sunset },
+        weather,
+        wind: { speed }
+    } = data;
+
+    const { main: details, icon } = weather[0];
+
+    return { lat, lon, temp, feels_like, temp_min, temp_max, humidity, name, dt, country, sunrise, sunset, weather, speed };
+};
+
+/*const formatForecastWeather = (data) => {
+    let {timezone, daily, hourly} = data;
+    daily = daily.slice(1,6).map();
+}*/
+
+const getFormattedWeatherData = async (searchParams) => {
+    const formattedCurrentWeather = await getWeatherData(
+            'weather', searchParams)
+        .then(formatCurrentWeather);
+
+        const {lat, lon} = formatCurrentWeather;
+
+    return formattedCurrentWeather;
+};
+
+const iconUrl = (code) => `http://openweathermap.org/img/wn/${code}@2x.png`
+
+/*const formattedForecastWeather = await getFormattedWeatherData('onecall', {
+    lat,
+    lon,
+    exclude: 'current, minutely, alerts',
+    units: searchParams.units
+}).then(formatForecastWeather);*/
+
+export default getFormattedWeatherData;
+
+export { iconUrl };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,12 +4,31 @@ import TopCities from '../components/TopCities'
 import CurrentDate from '../components/CurrentDate'
 import Divider from '../components/Divider'
 import "./Home.css"
+import getFormattedWeatherData from '../components/services/weatherServices'
+import { useEffect, useState } from 'react'
 
 const Home = () => {
+    
+    const [query, setQuery] = useState({q: 'Spisska Nova Ves'});
+    const [units, setUnits] = useState({units: 'metric'});
+    const [weather, setWeather] = useState(null);
+
+    useEffect (() => {
+        const fetchWeather = async () => {
+            await getFormattedWeatherData(query, units) //...query???
+            .then((data) => {
+                console.log(data)
+                setWeather(data);
+            });
+        }
+        fetchWeather();
+    }, [query, units]);
+ 
+
     return (
         <div className='search-container'>
             <TopCities />
-            <SearchBar />
+            <SearchBar setQuery={setQuery} />
             <Divider />
             <CurrentDate />
             <Video />
